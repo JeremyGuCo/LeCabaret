fb9f7ef0b38f4aaeaa0c75d2977692dbd696af88
Jérémy Guérin-Coquereau <jeremy@lifeaz.fr>
2024-10-26 16:44:18 +0200

    update : ambient music

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 window.addEventListener('load', () => {
   const curtainContainer = document.querySelector('.curtain-container');
+  const backgroundContainer = document.querySelector('.background-container');
   const container = document.querySelector('.container');
+  const backgroundMusic = document.getElementById('background-music');
   
   setTimeout(() => {
     curtainContainer.classList.add('open');
     setTimeout(() => {
       curtainContainer.style.display = 'none';
-      container.classList.add('visible');
-      document.body.style.overflow = 'auto'; // Réactive le défilement après l'animation
-    }, 1500); // Durée de l'animation des rideaux
+      backgroundContainer.classList.add('bg-visible');
+      setTimeout(() => {
+        container.classList.add('visible');
+        backgroundMusic.play(); // Démarre la lecture de la musique
+        document.body.style.overflow = 'auto'; // Réactive le défilement après l'animation
+      }, 2000); // Délai pour l'apparition du contenu après l'arrière-plan
+    }, 2000); // Durée de l'animation des rideaux
   }, 1000); // Délai avant le début de l'animation des rideaux
 });
